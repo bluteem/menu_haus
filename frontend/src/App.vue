bd9a0ef0f04a24f2d973755fbc9be7c417d63add
Abdullah Guc <gucabdullah@gmail.com>
2024-01-30 18:14:53 +0300

    commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<!-- App.vue -->
+<template>
+  <main class="container mx-auto mt-6">
+    <AddMenuItem />
+  </main>
+</template>
+
+<script setup>
+import AddMenuItem from './components/AddMenuItem.vue'
+</script>
