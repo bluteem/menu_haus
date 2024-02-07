b6a2e1162f287f1005d1e3e91a87a3587514188a
Abdullah Guc <gucabdullah@gmail.com>
2024-02-07 21:42:13 +0300

    commit

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,15 +16,97 @@
             </div>
         </div>
     </nav>
+
+    <div class="mx-auto max-w-xl mt-6 px-6">
+        <ul>
+          <li v-for="menuItem in menuItems" :key="menuItem._id" class="flex items-center border-b border-gray-300 pt-2 pb-4">
+            <!-- Left column for the image -->
+            <div class="w-32 mr-4">
+              <img :src="'/images/' + menuItem.images[0]" :alt="menuItem.name" class="w-full h-full object-cover rounded-md">
+            </div>
+            <!-- Right column for the text content -->
+            <div class="flex-grow">
+              <h2 class="text-xl font-semibold">{{ menuItem.name }}</h2>
+              <p class="text-gray-600"><span class="font-bold">Category:</span> {{ menuItem.category }}</p>
+              <p class="text-gray-600"><span class="font-bold">Price:</span> ${{ menuItem.price.toFixed(2) }}</p>
+              <!-- Edit and Delete buttons -->
+              <div class="flex">
+                <button @click="getMenuItem(menuItem._id)" type="button"
+                  class="mt-2 mr-2 px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300">Details</button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <!-- Show message if there are no menu items -->
+        <p v-if="menuItems.length === 0" class="text-gray-600">No menu items available</p>
+    </div>
+
+    <!-- Modal for adding a new menu item -->
+    <div :style="{ display: showModal1 ? 'block' : 'none' }" class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+            <!-- Modal content -->
+            <div
+            class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">{{ selectedMenuItem.name }}</h3>
+                <div class="mt-2">
+
+                    <carousel style="width: 100%; height: 100%;">
+                        <slide v-for="(image, index) in selectedMenuItem.images" :key="index">
+                            <div class="carousel__item">
+                                <img :src="'/images/' + image" :alt="selectedMenuItem.name" class="w-full h-full object-cover rounded-md">
+                            </div>
+                        </slide>
+                        <template #addons>
+                            <navigation />
+                            <pagination />
+                        </template>
+                    </carousel>
+             
+                    <p class="text-gray-600 mt-4"><span class="font-bold">Category:</span> {{ selectedMenuItem.category }}</p>
+                    <p class="text-gray-600"><span class="font-bold">Description:</span> {{ selectedMenuItem.description }}</p>
+                    <p class="text-gray-600"><span class="font-bold">Price:</span> {{ selectedMenuItem.price }}</p>
+
+                </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button @click="showModal1 = false" type="button"
+                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                Close
+                </button>
+            </div>
+            </div>
+        </div>
+    </div>    
+
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, defineComponent } from 'vue';
 import axios from 'axios';
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
+import '../../node_modules/vue3-carousel/dist/carousel.css';
 
-export default {
+export default defineComponent({
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
     setup() {
         const menuCategories = ref([]);
+        const menuItems = ref([]);
+        const showModal1 = ref(false);
+        const selectedMenuItem = ref({
+            name: '',
+            images: [],
+            category: '',
+            description: '',
+            price: null,
+        });
 
         // Fetch menu items when the component is mounted
         onMounted(async () => {
@@ -34,15 +116,72 @@ export default {
             } catch (error) {
                 console.error('Error fetching menu categories:', error);
             }
+            try {
+                const response = await axios.get('http://localhost:5000/api/menuitems');
+                menuItems.value = response.data.menuItems;
+            } catch (error) {
+                console.error('Error fetching menu items:', error);
+            }
         });
 
-        return {
-            menuCategories
+        // Fetch a single menu item
+        const getMenuItem = async (itemId) => {
+            try {
+                const response = await axios.get(`http://localhost:5000/api/menuitems/${itemId}`);
+                const menuItem = response.data.menuItem;
+                selectedMenuItem.value = {
+                    _id: menuItem._id,
+                    name: menuItem.name,
+                    images: menuItem.images,
+                    category: menuItem.category,
+                    description: menuItem.description,
+                    price: menuItem.price
+                };
+                showModal1.value = true;
+            } catch (error) {
+                console.error('Error fetching menu item:', error);
+                // Show alert if failed to fetch menu item details
+                alertRef.showAlert('Failed to fetch menu item details. Please try again later.');
+            }
         };
+        
+        const settings = ref({
+            itemsToShow: 1,
+            wrapAround: true,
+        });      
+
+        return {
+            menuCategories,
+            menuItems,
+            showModal1,
+            selectedMenuItem,
+            getMenuItem,
+            settings,
+        };       
     }
-};
+});
 </script>
 
 <style scoped>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.carousel__slide {
+  padding: 0px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
 </style>
