8617bd82c5710ad9158c8fcf9eeee8134e06ac05
Abdullah Guc <gucabdullah@gmail.com>
2024-02-13 14:38:14 +0300

    Create user.js

---
+++ server/models/user.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String // You may store the URL of the profile picture
+    },
+    accountStatus: {
+        type: String,
+        enum: ['active', 'inactive', 'suspended'],
+        default: 'active'
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    },
+    lastLogin: {
+        type: Date
+    }
+}, { collection: 'users' }); // Specify your collection name here
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
