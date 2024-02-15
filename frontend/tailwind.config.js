f06c7b62538bd7bab5a91d7ba4bb12aeaeb6c607
Abdullah Guc <gucabdullah@gmail.com>
2024-02-15 23:49:38 +0300

    commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,vue}",
-    "./node_modules/tw-elements/dist/js/**/*.js"
-  ],
-  theme: {
-    extend: {
-      scrollbar: {
-        thin: '1px',
-      },
-    },
-  },
-  darkMode: "class",
-  plugins: [
-    require("tw-elements/dist/plugin.cjs")
-  ],
-}
-
+	content: ["./src/**/*.{html,js,vue}", "./node_modules/tw-elements/dist/js/**/*.js"],
+	theme: {
+		extend: {
+			scrollbar: {
+				thin: "1px",
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [require("tw-elements/dist/plugin.cjs")],
+};
