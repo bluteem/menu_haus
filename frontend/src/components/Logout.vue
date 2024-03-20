b1bcea8da8c2740cff810f68e56dea4a298a92f3
Abdullah Guc <gucabdullah@gmail.com>
2024-03-20 14:57:05 +0300

    JWT token integrated

--- frontend/src/components/Logout.vue
+++ frontend/src/components/Logout.vue
@@ -14,29 +14,12 @@ export default {
 
 		const logout = async () => {
 			try {
-				const response = await axios.post(
-					"http://localhost:5000/auth/logout",
-					{},
-					{
-						withCredentials: true, // Send cookies with the request
-						timeout: 10000, // Set the timeout to 10 seconds (adjust as needed)
-					}
-				);
+				localStorage.removeItem("token"); // Replace with your token key
 
-				// Check for successful response (status code 200)
-				if (response.status === 200) {
-					console.log("Logout successful from backend");
-					// Clear local storage or other mechanisms if needed
-					localStorage.removeItem("token"); // Replace with your token key
-
-					router.push("/login").catch((error) => {
-						console.error("Navigation error:", error);
-						// Handle specific errors like 401 Unauthorized if needed
-					});
-				} else {
-					console.error("Unexpected response:", response.status, response.data);
-					// Handle unexpected responses (e.g., show error message)
-				}
+				router.push("/login").catch((error) => {
+					console.error("Navigation error:", error);
+					// Handle specific errors like 401 Unauthorized if needed
+				});
 			} catch (error) {
 				console.error("Logout failed", error);
 				// Handle network errors or other exceptions
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,8 +15,11 @@ router.post("/login", async (req, res) => {
 			return res.status(401).json({ message: "Invalid email or password" });
 		}
 
-		// Generate JWT token
-		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+		// Set expiration time for the token (e.g., 1 day)
+		const expiresIn = "1h";
+
+		// Generate JWT token with expiration time
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn });
 
 		// Return token in JSON response
 		return res.status(200).json({ token });
