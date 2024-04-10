5d315948e739fb45240b9cd7a788dfd171ec9e18
Abdullah Guc <gucabdullah@gmail.com>
2024-04-11 01:54:07 +0300

    commit

--- frontend/src/components/FrontendNavbar.vue
+++ frontend/src/components/FrontendNavbar.vue
@@ -28,53 +28,12 @@
 						type="button"
 						class="relative rounded-full bg-gray-800 mx-1 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 						<span class="absolute -inset-1.5"></span>
-						<span class="sr-only">View dish</span>
-						<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 502.4 502.4" aria-hidden="true">
-							<g>
-								<g>
-									<path
-										d="M272,193.81v-13.867c0-11.733-9.6-21.333-21.333-21.333s-21.333,9.6-21.333,21.333v13.867
-			c-106.667,11.733-188.8,108.8-188.8,226.133c0,11.733,9.601,21.333,21.334,21.333h377.6c11.733,0,21.333-9.6,21.333-21.333
-			C460.8,301.543,378.666,205.543,272,193.81z M83.2,399.677c9.6-92.8,80-164.267,166.4-164.267s157.867,71.467,167.467,164.267
-			H83.2z" />
-								</g>
-							</g>
-							<g>
-								<g>
-									<path
-										d="M481.067,459.41H21.333C9.6,459.41,0,469.01,0,480.743s9.6,21.333,21.333,21.333H480c11.733,0,21.333-9.6,22.4-21.333
-			C502.4,469.01,492.8,459.41,481.067,459.41z" />
-								</g>
-							</g>
-							<g>
-								<g>
-									<path
-										d="M110.934,157.544c1.067-1.067,1.067-2.133,2.133-4.267c3.2-5.333,7.467-12.8,7.467-23.467c0-11.733-3.2-20.267-6.4-26.667
-			c-5.333-12.8-12.8-21.333-21.333-24.533c-6.4-2.133-13.867-2.133-20.267,1.067c-9.6,5.333-14.933,17.067-9.6,27.733
-			c3.2,6.399,8.533,10.666,13.866,11.733v1.067c1.067,2.133,3.2,6.4,3.2,7.467c0,1.067-2.133,4.267-3.2,6.4
-			c-1.067,2.133-2.133,5.333-3.2,7.467c-8.533,20.267-6.4,46.933,5.333,64c4.267,6.4,10.667,9.6,17.067,9.6
-			c3.2,0,7.467-1.067,11.733-4.267c9.6-6.4,11.733-19.2,5.333-28.8C108.8,175.677,107.734,165.01,110.934,157.544z" />
-								</g>
-							</g>
-							<g>
-								<g>
-									<path
-										d="M266.667,80.743c1.067-1.067,1.067-3.2,2.133-4.267c3.2-5.333,7.467-12.8,7.467-23.467c0-11.733-3.2-20.267-6.4-26.667
-			c-5.333-12.8-12.8-21.333-21.333-24.533c-6.4-2.133-13.867-2.133-20.267,1.067c-9.6,5.333-14.933,17.067-9.6,27.733
-			c3.2,6.4,8.533,10.666,13.866,11.733c1.067,3.2,3.2,7.467,3.2,8.533c0,1.067-2.133,4.267-3.2,6.4
-			c-1.067,2.133-2.133,5.333-3.2,7.467c-8.533,20.267-6.4,46.933,5.333,64c4.267,6.4,10.667,9.6,17.067,9.6
-			c4.267,0,7.467-1.067,11.733-4.267c9.6-6.4,11.733-19.2,5.333-28.8C264.534,98.877,263.467,88.21,266.667,80.743z" />
-								</g>
-							</g>
-							<g>
-								<g>
-									<path
-										d="M436.267,208.744c-5.333-5.333-8.533-19.2-8.533-35.2c0-6.4,1.067-13.867,1.067-20.267
-			c1.067-11.733,2.133-23.467,1.067-35.2c-2.133-24.533-16-33.067-27.733-33.067c-11.733,0-21.333,9.6-21.333,21.333
-			c0,6.4,2.133,11.733,6.4,16c0,9.599,0,19.2-1.067,28.799c-1.067,8.533-1.067,16-1.067,23.467c0,19.2,3.2,44.8,19.2,62.933
-			c4.267,5.333,10.667,7.467,16,7.467c4.267,0,9.6-1.067,13.867-6.4C442.666,231.144,443.733,217.277,436.267,208.744z" />
-								</g>
-							</g>
+						<span class="sr-only">Search Menu</span>
+						<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M4 11C4 7.13401 7.13401 4 11 4C14.866 4 18 7.13401 18 11C18 14.866 14.866 18 11 18C7.13401 18 4 14.866 4 11ZM11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C13.125 20 15.078 19.2635 16.6177 18.0319L20.2929 21.7071C20.6834 22.0976 21.3166 22.0976 21.7071 21.7071C22.0976 21.3166 22.0976 20.6834 21.7071 20.2929L18.0319 16.6177C19.2635 15.078 20 13.125 20 11C20 6.02944 15.9706 2 11 2Z" />
 						</svg>
 					</button>
 
@@ -82,71 +41,28 @@
 						type="button"
 						class="relative rounded-full bg-gray-800 mx-1 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 						<span class="absolute -inset-1.5"></span>
-						<span class="sr-only">View notifications</span>
-						<svg
-							class="h-6 w-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true">
+						<span class="sr-only">View Basket</span>
+						<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M12 3C10.3432 3 9.00005 4.34315 9.00005 6H15C15 4.34315 13.6569 3 12 3ZM7.00005 6C7.00005 3.23858 9.23863 1 12 1C14.7615 1 17 3.23858 17 6H16.3441C16.7794 6.00005 17.1599 6.0013 17.4791 6.026C17.8369 6.05369 18.1919 6.11475 18.5417 6.27628C19.0471 6.50961 19.4776 6.87893 19.785 7.34294C19.9979 7.66418 20.1122 8.00569 20.194 8.35514C20.2709 8.68375 20.3324 9.08359 20.4031 9.54318L20.8541 12.4751C21.0468 13.7273 21.2014 14.7319 21.2546 15.5469C21.3091 16.3818 21.2669 17.1241 20.9938 17.8221C20.5817 18.8752 19.8247 19.7575 18.8465 20.325C18.1981 20.7011 17.4709 20.8556 16.6375 20.9287C15.8239 21 14.8074 21 13.5406 21H10.4595C9.19267 21 8.17621 21 7.36265 20.9287C6.52917 20.8556 5.80196 20.7011 5.15361 20.325C4.17539 19.7575 3.41842 18.8752 3.00631 17.8221C2.73317 17.1241 2.69104 16.3818 2.74554 15.5469C2.79873 14.732 2.9533 13.7273 3.14594 12.4752L3.59703 9.54315C3.66772 9.08358 3.72921 8.68375 3.80611 8.35514C3.88789 8.00569 4.0022 7.66418 4.21506 7.34294C4.52251 6.87893 4.953 6.50961 5.45837 6.27628C5.80824 6.11475 6.16316 6.05369 6.52098 6.026C6.84022 6.0013 7.22073 6.00005 7.656 6H7.00005ZM6.67528 8.02004C6.43801 8.0384 6.34578 8.06944 6.29671 8.09209C6.12826 8.16987 5.98476 8.29298 5.88228 8.44765C5.85243 8.4927 5.80772 8.57914 5.7535 8.81085C5.69635 9.05506 5.64603 9.3776 5.56836 9.88243L5.12984 12.7328C4.9284 14.0422 4.7881 14.9601 4.74129 15.6772C4.69513 16.3844 4.74974 16.789 4.86878 17.0932C5.11605 17.7251 5.57023 18.2545 6.15717 18.595C6.43973 18.7589 6.83136 18.8744 7.53736 18.9363C8.25323 18.9991 9.18181 19 10.5066 19H13.4935C14.8183 19 15.7469 18.9991 16.4627 18.9363C17.1687 18.8744 17.5604 18.7589 17.8429 18.595C18.4299 18.2545 18.884 17.7251 19.1313 17.0932C19.2504 16.789 19.305 16.3844 19.2588 15.6772C19.212 14.9601 19.0717 14.0422 18.8703 12.7328L18.4317 9.88243C18.3541 9.37761 18.3037 9.05507 18.2466 8.81085C18.1924 8.57914 18.1477 8.4927 18.1178 8.44765C18.0153 8.29298 17.8718 8.16987 17.7034 8.09209C17.6543 8.06944 17.5621 8.0384 17.3248 8.02004C17.0748 8.00069 16.7483 8 16.2376 8H7.76255C7.25178 8 6.92534 8.00069 6.67528 8.02004Z" />
 						</svg>
 					</button>
 
-					<!-- Profile dropdown -->
-					<div class="relative ml-3">
-						<div>
-							<button
-								type="button"
-								class="relative flex rounded-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-								id="user-menu-button"
-								aria-expanded="false"
-								aria-haspopup="true">
-								<span class="absolute -inset-1.5"></span>
-								<span class="sr-only">Open user menu</span>
-								<img
-									class="h-8 w-8 rounded-full"
-									src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-									alt="" />
-							</button>
-						</div>
-
-						<!-- <div
-							class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-							role="menu"
-							aria-orientation="vertical"
-							aria-labelledby="user-menu-button"
-							tabindex="-1">
-							<a
-								href="#"
-								class="block px-4 py-2 text-gray-700"
-								role="menuitem"
-								tabindex="-1"
-								id="user-menu-item-0"
-								>Your Profile</a
-							>
-							<a
-								href="#"
-								class="block px-4 py-2 text-gray-700"
-								role="menuitem"
-								tabindex="-1"
-								id="user-menu-item-1"
-								>Settings</a
-							>
-							<a
-								href="#"
-								class="block px-4 py-2 text-gray-700"
-								role="menuitem"
-								tabindex="-1"
-								id="user-menu-item-2"
-								>Sign out</a
-							>
-						</div> -->
-					</div>
+					<button
+						type="button"
+						class="relative flex rounded-full ml-1 bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+						id="user-menu-button"
+						aria-expanded="false"
+						aria-haspopup="true">
+						<span class="absolute -inset-1.5"></span>
+						<span class="sr-only">Open user menu</span>
+						<img
+							class="h-8 w-8 rounded-full"
+							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+							alt="" />
+					</button>
 				</div>
 			</div>
 		</div>
@@ -155,8 +71,18 @@
 		<div class="sm:hidden" id="mobile-menu">
 			<div class="space-y-1 px-2 pb-3 pt-2">
 				<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-				<a href="#" class="text-gray-300 bg-sky-700 hover:text-white block rounded-md px-3 py-2 text-base text-center">
-					Call a Waiter</a
+				<a
+					href="#"
+					class="text-gray-300 bg-sky-700 hover:text-white rounded-md px-3 py-2 flex items-center justify-center">
+					<svg class="h-6 w-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+						<path
+							d="M6 18V14M6 14H8L13 17V7L8 10H5C3.89543 10 3 10.8954 3 12V12C3 13.1046 3.89543 14 5 14H6ZM17 7L19 5M17 17L19 19M19 12H21"
+							stroke="currentColor"
+							stroke-width="1"
+							stroke-linecap="round"
+							stroke-linejoin="round" />
+					</svg>
+					<span class="align-middle">Call a Waiter</span></a
 				>
 			</div>
 		</div>
